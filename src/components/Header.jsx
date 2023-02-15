48c38da78f04bfaad8ac18b6dca329e8ab3a2819
özgebayram <ozgebayram1234@gmail.com>
2023-02-15 11:09:03 +0300

    feat: Card Component done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import Container from "react-bootstrap/Container"
-import  Image  from 'react-bootstrap/Image'
-import reactLogo from "../assets/react.svg"
-
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import reactLogo from "../assets/react.svg";
 const Header = () => {
   return (
-	<Container>
-		<Image fluid src={reactLogo} width="250px"></Image>
+    <Container>
+      <Image fluid src={reactLogo} width="250px"></Image>
+    </Container>
+  );
+};
 
-	</Container>
-  )
-}
-
-export default Header
+export default Header;
